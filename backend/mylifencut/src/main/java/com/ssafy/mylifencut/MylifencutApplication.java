0846ca5d6c3e4cf698e3560afbf43282a12275de
Il kwon <alexryu1105@naver.com>
2022-10-24 13:42:12 +0900

    chore<BE> : 프로젝트 생성

---
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/MylifencutApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.mylifencut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylifencutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylifencutApplication.class, args);
+	}
+
+}
