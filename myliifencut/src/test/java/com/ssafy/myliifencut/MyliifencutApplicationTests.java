4b19d38453c68e85d18ccbcb765ef25a1fb76bc1
Il kwon <alexryu1105@naver.com>
2022-10-24 13:37:08 +0900

    chore<BE> : 프로젝트 생성

---
+++ myliifencut/src/test/java/com/ssafy/myliifencut/MyliifencutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.myliifencut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyliifencutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
