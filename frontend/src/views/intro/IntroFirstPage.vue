ca7697f302efd5f85337a7b85fc424d3f9a55b39
kwanukwanu <xoxo70@naver.com>
2022-10-27 16:17:27 +0900

    feat <FE> 부트스트랩 추가 및 인트로 페이지 UI 구현

--- frontend/src/views/intro/IntroFirstPage.vue
+++ frontend/src/views/intro/IntroFirstPage.vue
@@ -2,18 +2,31 @@
   <video muted autoplay loop playbackRate="1.0">
     <source src="@/assets/intro_video.mp4" type="video/mp4">
   </video>
-  <div class="nav">
-    <span style="margin-right:2%">어서오세요, ㅇㅇㅇ님</span>
-    <router-link :to="{ name: 'traveldiary' }" class="nav-link">
-      나의 여행 일지
-    </router-link>
-    <router-link :to="{ name: 'galaxygallery' }" class="nav-link">
-      은하갤러리
-    </router-link>
-    <router-link :to="{ name: 'planetlist' }" class="nav-link">
-      행성 목록
-    </router-link>
+  <div style="color: #ffffff;  text-align: right;  padding-right:66px; margin-top: 3.5%;">어서오세요, <span style="font-weight:400">최재현</span>님</div>
+  <b-navbar toggleable="lg" style="float: right; margin-right: 50px;">
+    <b-navbar-nav>
+      <b-nav-item>
+        <router-link :to="{ name: 'traveldiary' }" class="nav-link">
+          나의 여행 일지
+        </router-link>
+      </b-nav-item>
+      <b-nav-item>
+        <router-link :to="{ name: 'galaxygallery' }" class="nav-link">
+          은하갤러리
+        </router-link>
+      </b-nav-item>
+      <b-nav-item>
+        <router-link :to="{ name: 'planetlist' }" class="nav-link">
+          행성 목록
+        </router-link>
+      </b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+  <br/><br/><br/><br/><br/>
+  <div style="text-align:center;">
+    <img src="@/assets/astronaut_riding.png" />
   </div>
+  <br/><br/><br/><br/>
   <div class="box">
     <div class="start-btn">
       <router-link :to="{ name: 'introstory1' }" class="nav-link" style="text-decoration: none; color: inherit;">
@@ -28,8 +41,40 @@
 </script>
 
 <style scoped>
+.navbar-expand-lg .navbar-nav .nav-link a:hover {
+  text-decoration: none;
+  color: #ffffff;
+  font-weight: 600;
+}
+
+.navbar-expand-lg .navbar-nav .nav-link {
+  color: #ffffff;
+}
+
 body {
   margin: 0;
+}
+
+img{
+  width:40%;
+  animation: bounce .75s infinite alternate;
+  -webkit-animation: bounce .75s infinite alternate;
+}
+@keyframes bounce{
+  from{
+    transform: translateY(0px);
+  }
+  to{
+    transform: translateY(-15px);
+  }
+}
+@-webkit-keyframes bounce{
+  from{
+    transform: translateY(0px);
+  }
+  to{
+    transform: translateY(-15px);
+  }
 }
 
 video {
@@ -44,22 +89,14 @@ video {
   background-size: cover;
 }
 
-.nav {
-  color: #ffffff;
-  text-align: right;
-  padding-right: 5%;
-  margin-top: 5%;
-}
-
 .nav-link {
   text-decoration: none;
   color: inherit;
   font-size: x-large;
-  margin-right: 2%;
+  padding: 0;
 }
 
 .start-btn {
-  margin-top: 35%;
   text-align: center;
   background-color: #ffffff;
   border-radius: 10px;
@@ -73,7 +110,17 @@ video {
 .box {
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+}
+
+.container,
+.container-fluid,
+.container-xxl,
+.container-xl,
+.container-lg,
+.container-md,
+.container-sm {
+  padding: 0
 }
 </style>
