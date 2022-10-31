150757d6160fc78d7e2f87af157b5def8c982497
DESKTOP-KVCQHCD\SSAFY <niinp@naver.com>
2022-10-31 16:58:03 +0900

    chore<FE>: masonry 라이브러리 설치 및 환경설정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,5 +5,6 @@ import { createPinia } from 'pinia'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import MasonryWall from '@yeger/vue-masonry-wall'
 
-createApp(App).use(createPinia()).use(router).use(BootstrapVue3).mount('#app')
+createApp(App).use(createPinia()).use(router).use(BootstrapVue3).use(MasonryWall).mount('#app')
