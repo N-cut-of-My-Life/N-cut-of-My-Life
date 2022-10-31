a96cb44a3ddbe3b97216457bbdeadbab5bfa0252
choijoohee <choijoohee@naver.com>
2022-10-31 16:34:04 +0900

    feat : AnswerRequest 구현

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/dto/AnswerRequest.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/dto/AnswerRequest.java
@@ -1,4 +1,15 @@
 package com.ssafy.mylifencut.answer.dto;
 
+import com.ssafy.mylifencut.answer.domain.State;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class AnswerRequest {
+	private Integer userId;
+	private Integer questionId;
+	private String contents;
+	private State state;
 }
