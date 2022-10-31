4a480f098531ef3ddf053c4cb56ccc91859d7592
choijoohee <choijoohee@naver.com>
2022-10-31 14:22:57 +0900

    test: 답변 등록 성공 테스트에 userId를 User 객체로 변경

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/answer/repository/AnswerRepositoryTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/answer/repository/AnswerRepositoryTest.java
@@ -19,17 +19,24 @@ class AnswerRepositoryTest {
 	@DisplayName("답변 등록 성공")
 	public void createAnswer() {
 		//given
+		final User user = User.builder()
+			.id(1)
+			.email("test@email.com")
+			.name("테스트")
+			.build();
+
 		final Answer answer = Answer.builder()
 			.id(1)
-			.userId(1)
+			.user(user)
 			.questionId(1)
 			.contents("답변 내용")
 			.state(State.CLOSE)
 			.build();
+
 		final Answer savedAnswer = answerRepository.save(answer);
 		assertNotNull(savedAnswer);
 		assertEquals(savedAnswer.getId(), 1);
-		assertEquals(savedAnswer.getUserId(), 1);
+		assertEquals(savedAnswer.getUser(), user);
 		assertEquals(savedAnswer.getQuestionId(), 1);
 		assertEquals(savedAnswer.getContents(), "답변 내용");
 		assertEquals(savedAnswer.getState(), State.CLOSE);
