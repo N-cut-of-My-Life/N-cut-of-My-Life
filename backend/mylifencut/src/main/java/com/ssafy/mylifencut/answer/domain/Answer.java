9b3e7dcf9649114f69d8cbe75ab507bfa36310ad
choijoohee <choijoohee@naver.com>
2022-10-31 14:17:08 +0900

    feat: Answer 엔티티 컬럼 변수 추가

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/domain/Answer.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/domain/Answer.java
@@ -1,16 +1,36 @@
 package com.ssafy.mylifencut.answer.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class Answer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	private Integer questionId;
+
+	private String contents;
+
+	@Enumerated(EnumType.STRING)
+	private State state;
 }
