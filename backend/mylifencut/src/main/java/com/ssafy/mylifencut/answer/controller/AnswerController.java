cb48fad6c34bf2f16893d28f9a33558f2e1c9283
Il Kwon <alexryu1105@naver.com>
2022-11-01 14:43:10 +0900

    feat : createAnswer answerService 호출

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/controller/AnswerController.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/controller/AnswerController.java
@@ -3,6 +3,7 @@ package com.ssafy.mylifencut.answer.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +11,17 @@ import com.ssafy.mylifencut.answer.dto.AnswerRequest;
 import com.ssafy.mylifencut.answer.service.AnswerService;
 import com.ssafy.mylifencut.common.dto.BaseResponse;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/answer")
+@RequiredArgsConstructor
 public class AnswerController {
 
-	private AnswerService answerService;
+	private final AnswerService answerService;
 	@PostMapping
-	public ResponseEntity<?> createAnswer(AnswerRequest answerRequest) {
-		//answerService.register(answerRequest);
+	public ResponseEntity<?> createAnswer(@RequestBody final AnswerRequest answerRequest) {
+		answerService.createAnswer(answerRequest);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
