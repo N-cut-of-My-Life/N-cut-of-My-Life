ae3597a9194b6ccb9703ec5e039ee4a01161e6f5
Il Kwon <alexryu1105@naver.com>
2022-11-01 12:56:29 +0900

    Revert "test : AnswerControllerTest MockMvc 설정"
    This reverts commit 7880f84ab67eff0c34a5d7e2f4b5d717a1988cd4.

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/answer/controller/AnswerControllerTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/answer/controller/AnswerControllerTest.java
@@ -1,35 +1,57 @@
 package com.ssafy.mylifencut.answer.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.google.gson.Gson;
+import com.ssafy.mylifencut.answer.domain.State;
+import com.ssafy.mylifencut.answer.dto.AnswerRequest;
+import com.ssafy.mylifencut.answer.service.AnswerService;
+import com.ssafy.mylifencut.common.dto.BaseResponse;
 
 @ExtendWith(MockitoExtension.class)
 class AnswerControllerTest {
 
-	@InjectMocks
-	private AnswerController answerController;
-	private MockMvc mockMvc;
-	private Gson gson;
+	@MockBean
+	AnswerService answerService;
 
-	@BeforeEach
-	public void init() {
-		mockMvc = MockMvcBuilders.standaloneSetup(answerController).build();
-	}
+	private MockMvc mockMvc;
 
 	@Test
-	@DisplayName("Mockmvc가 Null이 아님")
-	public void mockMvcIsNotNull() throws Exception {
-		assertNotNull(answerController);
-		assertNotNull(mockMvc);
+	@DisplayName("주어진 질문에 대한 답변을 등록한다.")
+	void createAnswer() throws Exception {
+		//given
+		AnswerRequest answerRequest = new AnswerRequest(3, 3, "답변 내용", State.OPEN);
+		BaseResponse baseResponse = BaseResponse.from(true, "답변이 등록되었습니다.");
+		given(answerService.register(answerRequest))
+			.willReturn(baseResponse);
+
+		//when
+		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/answer")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(new Gson().toJson(answerRequest))
+		);
+
+		//then
+		MvcResult mvcResult = resultActions.andExpect(status().isOk())
+			.andExpect(jsonPath("isSuccess", baseResponse.isSuccess()).exists())
+			.andExpect(jsonPath("message", baseResponse.getMessage()).exists())
+			.andExpect(jsonPath("data", baseResponse.getData()).doesNotExist())
+			.andReturn();
+
 	}
 }
