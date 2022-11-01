be9caa125f0fd9014967648dfc4cd217b3eaaf92
Il Kwon <alexryu1105@naver.com>
2022-11-01 15:53:16 +0900

    feat : BaseResponse 데이터 추가

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/common/dto/BaseResponse.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/common/dto/BaseResponse.java
@@ -1,13 +1,9 @@
 package com.ssafy.mylifencut.common.dto;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
 @Builder
@@ -22,16 +18,7 @@ public class BaseResponse {
 		return builder().isSuccess(isSuccess).message(message).build();
 	}
 
-	// public static ResponseEntity<?> success(String message, Object data) {
-	// 	return ResponseEntity.ok(new BaseResponse(true, message, data));
-	// }
-	//
-	// public static ResponseEntity<?> success(String message) {
-	// 	return ResponseEntity.ok(new BaseResponse(true, message));
-	// }
-	//
-	// public static ResponseEntity<?> error(String message, HttpStatus httpStatus) {
-	// 	return new ResponseEntity<>(new BaseResponse(false, message), httpStatus);
-	// }
-
+	public static BaseResponse from(boolean isSuccess, String message, Object data) {
+		return builder().isSuccess(isSuccess).message(message).data(data).build();
+	}
 }
