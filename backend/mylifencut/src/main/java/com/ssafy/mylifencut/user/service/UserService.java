de12e9745b7d7ad6a1f1f87ae82ae7dddfb95901
Il Kwon <alexryu1105@naver.com>
2022-11-03 00:43:08 +0900

    feat<BE>: UserService getAccessToken 함수 생성

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/service/UserService.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/service/UserService.java
@@ -2,6 +2,18 @@ package com.ssafy.mylifencut.user.service;
 
 import org.springframework.stereotype.Service;
 
+import com.ssafy.mylifencut.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+	private final UserRepository userRepository;
+
+	public String getAccessToken(String code) {
+		return null;
+	}
+
 }
