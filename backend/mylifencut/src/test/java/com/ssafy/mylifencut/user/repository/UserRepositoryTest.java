08b0a45761d848ef1d0cdf075386d3534440c897
Il Kwon <alexryu1105@naver.com>
2022-11-02 16:35:26 +0900

    test<BE>: 회원 저장 성공 테스트

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/repository/UserRepositoryTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/repository/UserRepositoryTest.java
@@ -2,10 +2,14 @@ package com.ssafy.mylifencut.user.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Collections;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.ssafy.mylifencut.user.domain.User;
 
 @DataJpaTest
 public class UserRepositoryTest {
@@ -13,8 +17,18 @@ public class UserRepositoryTest {
 	private UserRepository userRepository;
 
 	@Test
-	@DisplayName("UserRepository 생성 확인")
-	public void userRepositoryIsNotNull() {
-		assertNotNull(userRepository);
+	@DisplayName("회원 저장 성공")
+	public void register() {
+		final User user = User.builder()
+			.name("홍길동")
+			.email("ssafy@email.com")
+			.articles(Collections.emptyList())
+			.build();
+
+		final User result = userRepository.save(user);
+
+		assertEquals(user.getName(), result.getName());
+		assertEquals(user.getEmail(), result.getEmail());
+		assertEquals(user.getArticles(), result.getArticles());
 	}
 }
