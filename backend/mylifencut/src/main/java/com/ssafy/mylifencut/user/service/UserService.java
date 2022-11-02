779a9011551aa4dcf1e63f151328364994a6efd4
Il Kwon <alexryu1105@naver.com>
2022-11-03 01:29:14 +0900

    feat<BE>: UserService getAccessToken 함수 작성

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/service/UserService.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/service/UserService.java
@@ -1,19 +1,89 @@
 package com.ssafy.mylifencut.user.service;
 
+import static com.ssafy.mylifencut.user.UserConstant.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.ssafy.mylifencut.user.exception.InvalidAccessTokenException;
 import com.ssafy.mylifencut.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
 	private final UserRepository userRepository;
 
+	@Value("${oauth2.kakao.restApiKey}")
+	private final String kakao_restApiKey;
+
+	@Value("${oauth2.kakao.redirectUri}")
+	private final String kakao_redirectUri;
+
 	public String getAccessToken(String code) {
-		return null;
+		String access_Token = "";
+		String refresh_Token = "";
+		String reqURL = "https://kauth.kakao.com/oauth/token";
+
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+			//POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+
+			//POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+			String sb = "grant_type=authorization_code" + "&client_id=" + kakao_restApiKey + "&redirect_uri="
+				+ kakao_redirectUri
+				+ "&code=" + code;
+			bw.flush();
+
+			//결과 코드가 200이라면 성공
+			int responseCode = conn.getResponseCode();
+			log.info("responseCode : " + responseCode);
+
+			//요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = "";
+			String result = "";
+
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+			log.info("response body : " + result);
+
+			//Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+			JsonElement element = JsonParser.parseString(result);
+
+			access_Token = element.getAsJsonObject().get("access_token").getAsString();
+			refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
+
+			log.info("access_token : " + access_Token);
+			log.info("refresh_token : " + refresh_Token);
+
+			br.close();
+			bw.close();
+		} catch (IOException e) {
+			throw new InvalidAccessTokenException(INVALID_ACCESS_TOKEN_ERROR_MESSAGE);
+		}
+
+		return access_Token;
 	}
 
 }
