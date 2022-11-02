9b73e576bb3557214863affb8328c4cb7a55c880
sungsoo <tjdtn6725@naver.com>
2022-11-02 17:51:17 +0900

    feat : AnswerController 좋아요 추가 Api 개발

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/controller/AnswerController.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/controller/AnswerController.java
@@ -1,9 +1,11 @@
 package com.ssafy.mylifencut.answer.controller;
 
 import static com.ssafy.mylifencut.answer.AnswerConstant.*;
+import static com.ssafy.mylifencut.like.LikeConstant.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.mylifencut.answer.dto.AnswerRequest;
 import com.ssafy.mylifencut.answer.service.AnswerService;
 import com.ssafy.mylifencut.common.dto.BaseResponse;
+import com.ssafy.mylifencut.like.service.LikeService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,11 +24,20 @@ import lombok.RequiredArgsConstructor;
 public class AnswerController {
 
 	private final AnswerService answerService;
+	private final LikeService likeService;
 
 	@PostMapping
 	public ResponseEntity<?> createAnswer(@RequestBody final AnswerRequest answerRequest) {
 		return new ResponseEntity<>(
 			BaseResponse.from(true, CREATE_ANSWER_SUCCESS_MESSAGE, answerService.createAnswer(answerRequest)),
+			HttpStatus.OK);
+	}
+
+	@PostMapping("/{answer_id}/{user_id}")
+	public ResponseEntity<?> createLike(@PathVariable("answer_id") Integer answerId,
+		@PathVariable("user_id") Integer userId) {
+		return new ResponseEntity<>(
+			BaseResponse.from(true, CREATE_LIKE_SUCCESS_MESSAGE, likeService.createLike(userId, answerId)),
 			HttpStatus.OK);
 	}
 }
