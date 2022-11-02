078cd171d8bff9d0e4a6c510fec3ed30a3e1e4b7
choijoohee <choijoohee@naver.com>
2022-11-02 14:31:46 +0900

    test<BE>: 마이페이지 과거 기록 사이즈가 2일때 테스트 케이스 작성

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/repository/MyPageRepositoryTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/repository/MyPageRepositoryTest.java
@@ -2,6 +2,9 @@ package com.ssafy.mylifencut.user.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import com.ssafy.mylifencut.article.domain.Article;
+import com.ssafy.mylifencut.article.repository.ArticleRepository;
 import com.ssafy.mylifencut.user.domain.User;
 
 @DataJpaTest
@@ -17,6 +22,9 @@ public class MyPageRepositoryTest {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private ArticleRepository articleRepository;
+
 	@Test
 	public void repositoryIsNotNull() {
 		assertNotNull(userRepository);
@@ -24,12 +32,9 @@ public class MyPageRepositoryTest {
 
 	@Test
 	@DisplayName("마이페이지에서 과거 기록 리스트가 없을 때(사이즈가 0이어야함) - 성공")
-	public void RetrieveMyPage() {
+	public void RetrieveMyPageEmpty() {
 		//given
-		final User user = User.builder()
-			.email("test@email.com")
-			.name("이름")
-			.build();
+		final User user = user();
 
 		//when
 		final User savedUser = userRepository.save(user);
@@ -42,5 +47,49 @@ public class MyPageRepositoryTest {
 		assertNotNull(foundUser.get());
 		assertEquals(savedUser.getId(), foundUser.get().getId());
 		assertEquals(foundUser.get().getArticles().size(), 0);
+	}
+
+	@Test
+	@DisplayName("마이페이지에서 과거 기록 사이즈가 2이어야함 - 성공")
+	public void RetrieveMyPageSizeTwo() {
+		//given
+		final User user = user();
+		final Article article1 = Article.builder()
+			.user(user)
+			.answers(Collections.emptyList())
+			.createDate(LocalDateTime.now())
+			.build();
+		final Article article2 = Article.builder()
+			.user(user)
+			.answers(Collections.emptyList())
+			.createDate(LocalDateTime.now())
+			.build();
+		articleRepository.save(article1);
+		articleRepository.save(article2);
+
+		//when
+		user.addArticles(article1);
+		user.addArticles(article2);
+		final User savedUser = userRepository.save(user);
+
+		//then
+		assertNotNull(savedUser);
+		final Optional<User> foundUser = userRepository.findById(savedUser.getId());
+		assertTrue(foundUser.isPresent());
+		assertNotNull(foundUser.get());
+		assertEquals(savedUser.getId(), foundUser.get().getId());
+
+		final List<Article> articles = foundUser.get().getArticles();
+		assertEquals(articles.size(), 2);
+		for (Article article : articles) {
+			assertEquals(article.getUser().getId(), savedUser.getId());
+		}
+	}
+
+	private User user() {
+		return User.builder()
+			.email("test@email.com")
+			.name("이름")
+			.build();
 	}
 }
