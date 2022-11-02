4b872e042c9b3429d32e325ae2d1c18ae6e55b19
choijoohee <choijoohee@naver.com>
2022-11-02 13:38:57 +0900

    test<BE>: 마이페이지에서 과거기록 리스트가 없을 때 테스트 케이스 작성

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/repository/MyPageRepositoryTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/repository/MyPageRepositoryTest.java
@@ -2,19 +2,28 @@ package com.ssafy.mylifencut.user.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.ssafy.mylifencut.user.domain.User;
 
+@DataJpaTest
 public class MyPageRepositoryTest {
 
 	@Autowired
 	private UserRepository userRepository;
 
 	@Test
-	@DisplayName("마이페이지에서 과거 기록 사이즈가 0이어야함")
+	public void repositoryIsNotNull() {
+		assertNotNull(userRepository);
+	}
+
+	@Test
+	@DisplayName("마이페이지에서 과거 기록 리스트가 없을 때(사이즈가 0이어야함) - 성공")
 	public void RetrieveMyPage() {
 		//given
 		final User user = User.builder()
@@ -27,9 +36,11 @@ public class MyPageRepositoryTest {
 
 		//then
 		assertNotNull(savedUser);
-		final User foundUser = userRepository.findById(savedUser.getId());
-		assertNotNull(foundUser);
-		assertEquals(savedUser.getId(), foundUser.getId());
-		assertEquals(foundUser.getArticles().size(), 0);
+
+		final Optional<User> foundUser = userRepository.findById(savedUser.getId());
+		assertTrue(foundUser.isPresent());
+		assertNotNull(foundUser.get());
+		assertEquals(savedUser.getId(), foundUser.get().getId());
+		assertEquals(foundUser.get().getArticles().size(), 0);
 	}
 }
