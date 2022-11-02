28937f22316d316da6324b30fd9e0dad0df70627
sungsoo <tjdtn6725@naver.com>
2022-11-02 14:33:08 +0900

    feat: LikeRepository Interface -  findByUserIdAndAnswerId 메소드 구현

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/like/repository/LikeRepository.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/like/repository/LikeRepository.java
@@ -2,7 +2,8 @@ package com.ssafy.mylifencut.like.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.ssafy.mylifencut.like.domain.isLike;
+import com.ssafy.mylifencut.like.domain.IsLike;
 
-public interface LikeRepository extends JpaRepository<isLike, Integer> {
+public interface LikeRepository extends JpaRepository<IsLike, Integer> {
+	IsLike findByUserIdAndAnswerId(Integer userId, Integer answerId);
 }
