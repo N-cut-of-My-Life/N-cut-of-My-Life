041b590f38d6bb836da0357873dfb11224195275
sungsoo <tjdtn6725@naver.com>
2022-11-02 11:17:27 +0900

    feat: Answer 엔티티에 likes 컬럼 추가

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/domain/Answer.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/domain/Answer.java
@@ -1,5 +1,7 @@
 package com.ssafy.mylifencut.answer.domain;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -9,9 +11,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.ssafy.mylifencut.answer.dto.AnswerRequest;
 import com.ssafy.mylifencut.article.domain.Article;
+import com.ssafy.mylifencut.like.domain.isLike;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -38,6 +42,9 @@ public class Answer {
 
 	@Enumerated(EnumType.STRING)
 	private State state;
+
+	@OneToMany(mappedBy = "answer")
+	private List<isLike> likes;
 
 	public static Answer from(AnswerRequest answerRequest, Article article) {
 
