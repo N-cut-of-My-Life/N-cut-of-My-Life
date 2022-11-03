632703966f0c028b493bdb2f086976f7ffd36388
sungsoo <tjdtn6725@naver.com>
2022-11-03 16:57:34 +0900

    feat : Like 가존재하지 않을 때 삭제하는 경우 발생하는 NotExistLikeException > Exception Handler 등록

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/common/aop/ExceptionAdvice.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/common/aop/ExceptionAdvice.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.ssafy.mylifencut.answer.exception.InvalidStateException;
 import com.ssafy.mylifencut.common.dto.BaseResponse;
 import com.ssafy.mylifencut.like.exception.AlreadyLikeException;
+import com.ssafy.mylifencut.like.exception.NotExistLikeException;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
@@ -20,6 +21,13 @@ public class ExceptionAdvice {
 
 	@ExceptionHandler(AlreadyLikeException.class)
 	public ResponseEntity<?> alreadyLikeException(AlreadyLikeException exception) {
+		return new ResponseEntity<>(
+			BaseResponse.from(false, exception.getMessage()),
+			HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(NotExistLikeException.class)
+	public ResponseEntity<?> notExistLikeException(NotExistLikeException exception) {
 		return new ResponseEntity<>(
 			BaseResponse.from(false, exception.getMessage()),
 			HttpStatus.BAD_REQUEST);
