50a4a3fd5e0177add2ec1246f1bdba507576c589
sungsoo <tjdtn6725@naver.com>
2022-11-03 10:08:00 +0900

    feat : Exception Handler - return 값 BaseResponse 형식에 맞춰 반환하도록 수정.

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/common/aop/ExceptionAdvice.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/common/aop/ExceptionAdvice.java
@@ -6,17 +6,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.ssafy.mylifencut.answer.exception.InvalidStateException;
+import com.ssafy.mylifencut.common.dto.BaseResponse;
 import com.ssafy.mylifencut.like.exception.AlreadyLikeException;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
 	@ExceptionHandler(InvalidStateException.class)
-	public ResponseEntity<String> invalidStateException(InvalidStateException exception) {
-		return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+	public ResponseEntity<?> invalidStateException(InvalidStateException exception) {
+		return new ResponseEntity<>(
+			BaseResponse.from(false, exception.getMessage()),
+			HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(AlreadyLikeException.class)
-	public ResponseEntity<String> alreadyLikeException(AlreadyLikeException exception) {
-		return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+	public ResponseEntity<?> alreadyLikeException(AlreadyLikeException exception) {
+		return new ResponseEntity<>(
+			BaseResponse.from(false, exception.getMessage()),
+			HttpStatus.BAD_REQUEST);
 	}
 }
