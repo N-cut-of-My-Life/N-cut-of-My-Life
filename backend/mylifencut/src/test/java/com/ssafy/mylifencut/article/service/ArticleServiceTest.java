0d0de3cdbcf2a71a0e73e8c4e3c2ffaa6bd198b7
choijoohee <choijoohee@naver.com>
2022-11-03 14:44:14 +0900

    test<BE>: 여행일지 등록 실패 테스트 코드 버그 수정 - userId 값을 파라미터로 보내줘야함

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/article/service/ArticleServiceTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/article/service/ArticleServiceTest.java
@@ -3,7 +3,9 @@ package com.ssafy.mylifencut.article.service;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.ssafy.mylifencut.article.domain.Article;
+import com.ssafy.mylifencut.article.dto.ArticleRegisterRequest;
 import com.ssafy.mylifencut.article.dto.ArticleRetrieveResponse;
 import com.ssafy.mylifencut.article.repository.ArticleRepository;
 import com.ssafy.mylifencut.user.UserConstant;
@@ -35,7 +38,7 @@ class ArticleServiceTest {
 
 	@Test
 	@DisplayName("[여행일지 조회 실패] - 존재하지않는 UserId일 때")
-	public void notFoundUserError() {
+	public void retrieveArticle_notFoundUserError() {
 		//given
 		final int userId = 5;
 		doReturn(Optional.empty()).when(userRepository).findById(userId);
@@ -50,7 +53,7 @@ class ArticleServiceTest {
 
 	@Test
 	@DisplayName("[여행일지 조회 성공]")
-	public void retrieveArticleSuccess() {
+	public void retrieveArticle_success() {
 		//given
 		final int userId = 5;
 		final String userName = "여행일지유저";
@@ -76,13 +79,16 @@ class ArticleServiceTest {
 
 	@Test
 	@DisplayName("[여행일지 등록 실패] - 존재하지 않는 userId일 때")
-	public void registerArticle() {
+	public void registerArticle_notFoundUserError() {
 		//given
-		doReturn(Optional.empty()).when(userRepository).findById(anyInt());
+		doReturn(Optional.empty()).when(userRepository).findById(5);
 
 		//when
 		final UserNotFoundException result = assertThrows(UserNotFoundException.class
-			, () -> articleService.createArticle(any(ArticleRegisterRequest.class)));
+			, () -> articleService.createArticle(new ArticleRegisterRequest(
+				1, User.builder().id(5).build(), Collections.emptyList(), LocalDateTime.now())
+			)
+		);
 
 		//then
 		assertEquals(result.getMessage(), UserConstant.USER_NOT_FOUND_ERROR_MESSAGE);
