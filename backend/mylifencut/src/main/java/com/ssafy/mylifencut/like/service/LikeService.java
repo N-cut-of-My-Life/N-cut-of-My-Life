b5a3127d49567d40b5045dcf9cbaa858e5113729
sungsoo <tjdtn6725@naver.com>
2022-11-03 14:25:20 +0900

    feat : 좋아요 삭제 deleteLike Service 구현

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/like/service/LikeService.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/like/service/LikeService.java
@@ -1,12 +1,14 @@
 package com.ssafy.mylifencut.like.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.ssafy.mylifencut.answer.domain.Answer;
-import com.ssafy.mylifencut.like.LikeConstant;
 import com.ssafy.mylifencut.like.domain.IsLike;
 import com.ssafy.mylifencut.like.dto.IsLikeResponse;
 import com.ssafy.mylifencut.like.exception.AlreadyLikeException;
+import com.ssafy.mylifencut.like.exception.NotExistLikeException;
 import com.ssafy.mylifencut.like.repository.LikeRepository;
 import com.ssafy.mylifencut.user.domain.User;
 
@@ -19,18 +21,22 @@ public class LikeService {
 	private final LikeRepository likeRepository;
 
 	public IsLikeResponse createLike(Integer userId, Integer answerId) {
-		final IsLike result = likeRepository.findByUserIdAndAnswerId(userId, answerId);
-		if (result != null) {
-			throw new AlreadyLikeException(LikeConstant.ALREADY_LIKE_EXIST_ERROR_MESSAGE);
+		final Optional<IsLike> result = likeRepository.findByUserIdAndAnswerId(userId, answerId);
+		if (result.isPresent()) {
+			throw new AlreadyLikeException();
 		}
 		final IsLike isLike = IsLike.builder()
 			.user(User.builder().id(userId).build())
 			.answer(Answer.builder().id(answerId).build())
 			.build();
-
 		final IsLike savedIsLike = likeRepository.save(isLike);
 		IsLikeResponse isLikeResponse = IsLikeResponse.of(savedIsLike);
-
 		return isLikeResponse;
+	}
+
+	public void deleteLike(Integer userId, Integer answerId) {
+		final IsLike isLike = likeRepository.findByUserIdAndAnswerId(userId, answerId).orElseThrow(
+			NotExistLikeException::new);
+		likeRepository.delete(isLike);
 	}
 }
