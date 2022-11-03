5812d31a271b6dc6e6e005a4ce61bcde8d1f7585
sungsoo <tjdtn6725@naver.com>
2022-11-03 16:58:02 +0900

    feat : 좋아요 취소시 반환하는 메세지 상수 추가

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/like/LikeConstant.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/like/LikeConstant.java
@@ -3,6 +3,7 @@ package com.ssafy.mylifencut.like;
 public class LikeConstant {
 
 	public static final String CREATE_LIKE_SUCCESS_MESSAGE = "좋아요가 등록되었습니다.";
+	public static final String DELETE_LIKE_SUCCESS_MESSAGE = "좋아요가 취소되었습니다.";
 
 	public static final String ALREADY_LIKE_EXIST_ERROR_MESSAGE = "이미 좋아요가 등록된 답변입니다.";
 	public static final String NOT_EXIST_LIKE_ERROR_MESSAGE = "좋아요를 누르지 않은 답변이므로 취소할 수 없습니다";
