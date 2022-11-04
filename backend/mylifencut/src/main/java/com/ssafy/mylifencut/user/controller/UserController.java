017cd2e066f6bc9cca5a9bd4205f5a5977f93743
Il kwon <alexryu1105@naver.com>
2022-11-04 14:07:00 +0900

    fix<BE>: 로그인 JWT token 반환하게 변경 및 로그인 성공 상수 추가

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/controller/UserController.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.ssafy.mylifencut.user.controller;
 
+import static com.ssafy.mylifencut.user.UserConstant.*;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.mylifencut.common.dto.BaseResponse;
+import com.ssafy.mylifencut.user.JwtTokenProvider;
 import com.ssafy.mylifencut.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,10 +21,11 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
 	private final UserService userService;
+	private final JwtTokenProvider jwtTokenProvider;
 
 	@PostMapping("/login")
 	public ResponseEntity<BaseResponse> kakaoLogin(@RequestBody String accessToken) {
-		Integer userId = userService.kakaoLogin(accessToken);
-		return new ResponseEntity<>(BaseResponse.from(true, "성공", userId), HttpStatus.OK);
+		String jwtToken = jwtTokenProvider.createToken(userService.kakaoLogin(accessToken));
+		return new ResponseEntity<>(BaseResponse.from(true, KAKAO_LOGIN_SUCCESS_MESSAGE, jwtToken), HttpStatus.OK);
 	}
 }
