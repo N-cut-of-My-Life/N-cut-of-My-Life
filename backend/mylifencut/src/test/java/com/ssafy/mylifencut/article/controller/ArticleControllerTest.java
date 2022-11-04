55f7c8c2684dd364b4bcd2b51e9f930cfa89141b
choijoohee <choijoohee@naver.com>
2022-11-04 16:46:32 +0900

    test<BE>: 여행일지 조회 성공 테스트 케이스 작성

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/article/controller/ArticleControllerTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/article/controller/ArticleControllerTest.java
@@ -5,6 +5,10 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,10 +23,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.google.gson.Gson;
+import com.ssafy.mylifencut.article.ArticleConstant;
+import com.ssafy.mylifencut.article.dto.ArticleResponse;
 import com.ssafy.mylifencut.article.service.ArticleService;
 import com.ssafy.mylifencut.common.aop.ExceptionAdvice;
 import com.ssafy.mylifencut.common.dto.BaseResponse;
 import com.ssafy.mylifencut.user.UserConstant;
+import com.ssafy.mylifencut.user.domain.User;
 import com.ssafy.mylifencut.user.exception.UserNotFoundException;
 
 @ExtendWith(MockitoExtension.class)
@@ -68,5 +75,40 @@ class ArticleControllerTest {
 		assertNull(response.getData());
 		assertFalse(response.isSuccess());
 		assertEquals(UserConstant.USER_NOT_FOUND_ERROR_MESSAGE, response.getMessage());
+	}
+
+	@Test
+	@DisplayName("[여행일지 조회 성공]")
+	public void retrieveArticle_success() throws Exception {
+		//given
+		final int userId = 5;
+		final String url = "/article/" + userId;
+
+		final List<ArticleResponse> articleResponses = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			articleResponses.add(ArticleResponse.builder()
+				.id(i + 1)
+				.user(User.builder().id(userId).build())
+				.createDate(LocalDateTime.now())
+				.build());
+		}
+		doReturn(articleResponses).when(articleService).retrieveArticles(userId);
+
+		//when
+		final ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(url));
+
+		resultActions.andExpect(status().isOk());
+		final BaseResponse response = gson.fromJson(resultActions.andReturn()
+			.getResponse()
+			.getContentAsString(StandardCharsets.UTF_8), BaseResponse.class);
+
+		assertNotNull(response);
+		assertTrue(response.isSuccess());
+		assertEquals(ArticleConstant.RETRIEVE_SUCCESS_MESSAGE, response.getMessage());
+		List<ArticleResponse> list = (List<ArticleResponse>)response.getData();
+		for (int i = 0; i < list.size(); i++) {
+			Map articleResponse = (Map)list.get(i);
+			assertEquals((double)(i + 1), articleResponse.get("id"));
+		}
 	}
 }
