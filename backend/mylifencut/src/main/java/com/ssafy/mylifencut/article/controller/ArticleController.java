dfc8d48ba1b86e27fc7c274a3109bde8e787ddc2
choijoohee <choijoohee@naver.com>
2022-11-04 16:47:21 +0900

    feat<BE>: 여행일지 등록 성공 controller 프로덕션 코드 작성

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/article/controller/ArticleController.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/article/controller/ArticleController.java
@@ -1,5 +1,7 @@
 package com.ssafy.mylifencut.article.controller;
 
+import static com.ssafy.mylifencut.article.ArticleConstant.*;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.mylifencut.article.ArticleConstant;
 import com.ssafy.mylifencut.article.service.ArticleService;
 import com.ssafy.mylifencut.common.dto.BaseResponse;
 
@@ -19,13 +20,12 @@ import lombok.RequiredArgsConstructor;
 public class ArticleController {
 	private final ArticleService articleService;
 
-	@GetMapping("{userId}")
-	public ResponseEntity<BaseResponse> getArticle(@PathVariable int userId) {
-		return new ResponseEntity<>(
-			BaseResponse.from(
-				true
-				, ArticleConstant.RETRIEVE_SUCCESS_MESSAGE
-				, articleService.retrieveArticles(userId))
+	@GetMapping("/{userId}")
+	public ResponseEntity<BaseResponse> getArticle(@PathVariable("userId") Integer userId) {
+		return new ResponseEntity<>(BaseResponse.from(
+			true,
+			RETRIEVE_SUCCESS_MESSAGE,
+			articleService.retrieveArticles(userId))
 			, HttpStatus.OK);
 	}
 }
