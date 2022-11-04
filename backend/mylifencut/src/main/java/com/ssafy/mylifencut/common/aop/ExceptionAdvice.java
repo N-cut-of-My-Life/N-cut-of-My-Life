6a54e199c3ab687341c4468523d4df57ef6d1d70
choijoohee <choijoohee@naver.com>
2022-11-04 14:52:57 +0900

    refactor<BE>: ExceptionHandler 메서드의 내용이 중복되어 여러개 사용자정의Exception클래스를 하나의 부모클래스로 묶도록 수정

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/common/aop/ExceptionAdvice.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/common/aop/ExceptionAdvice.java
@@ -5,40 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.ssafy.mylifencut.answer.exception.InvalidStateException;
 import com.ssafy.mylifencut.common.dto.BaseResponse;
-import com.ssafy.mylifencut.like.exception.AlreadyLikeException;
-import com.ssafy.mylifencut.like.exception.NotExistLikeException;
-import com.ssafy.mylifencut.user.exception.UserNotFoundException;
+import com.ssafy.mylifencut.common.exception.BadRequestException;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
-	@ExceptionHandler(InvalidStateException.class)
-	public ResponseEntity<BaseResponse> invalidStateException(InvalidStateException exception) {
-		return new ResponseEntity<>(
-			BaseResponse.from(false, exception.getMessage()),
-			HttpStatus.BAD_REQUEST);
-	}
-
-	@ExceptionHandler(AlreadyLikeException.class)
-	public ResponseEntity<BaseResponse> alreadyLikeException(AlreadyLikeException exception) {
-		return new ResponseEntity<>(
-			BaseResponse.from(false, exception.getMessage()),
-			HttpStatus.BAD_REQUEST);
-	}
-
-	@ExceptionHandler(UserNotFoundException.class)
-	public ResponseEntity<BaseResponse> userNotFoundException(UserNotFoundException exception) {
+	@ExceptionHandler(BadRequestException.class)
+	public ResponseEntity<BaseResponse> alreadyLikeException(BadRequestException exception) {
 		return new ResponseEntity<>(
 			BaseResponse.from(false, exception.getMessage()),
 			HttpStatus.BAD_REQUEST
 		);
-	}
-
-	@ExceptionHandler(NotExistLikeException.class)
-	public ResponseEntity<?> notExistLikeException(NotExistLikeException exception) {
-		return new ResponseEntity<>(
-			BaseResponse.from(false, exception.getMessage()),
-			HttpStatus.BAD_REQUEST);
 	}
 }
--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/like/exception/NotExistLikeException.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/like/exception/NotExistLikeException.java
@@ -1,8 +1,9 @@
 package com.ssafy.mylifencut.like.exception;
 
+import com.ssafy.mylifencut.common.exception.BadRequestException;
 import com.ssafy.mylifencut.like.LikeConstant;
 
-public class NotExistLikeException extends RuntimeException {
+public class NotExistLikeException extends BadRequestException {
 	public NotExistLikeException() {
 		super(LikeConstant.NOT_EXIST_LIKE_ERROR_MESSAGE);
 	}
