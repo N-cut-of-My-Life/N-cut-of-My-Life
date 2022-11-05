1fad1d54304ced5fa88cabd34af651db37ee9fd3
SungSoo <tjdtn6725@naver.com>
2022-11-05 15:49:49 +0900

    test : AnswerRepositoryTest 연관된 테스트끼리 @Nested

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/answer/repository/AnswerRepositoryTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/answer/repository/AnswerRepositoryTest.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -55,52 +56,63 @@ class AnswerRepositoryTest {
 		assertEquals(savedAnswer.getState(), State.CLOSE);
 	}
 
-	@Test
-	@DisplayName("갤러리 조회 실패 - 등록된 답변이 없음")
-	public void answerListSizeZero() {
-		//given
-		//when
-		List<Answer> result = answerRepository.findAll();
-		//then
-		assertThat(result.size()).isEqualTo(0);
+	@Nested
+	@DisplayName("갤러리 조회 실패 테스트")
+	class GalleryReadFailTest{
+		@Test
+		@DisplayName("갤러리 조회 실패 - 등록된 답변이 없음")
+		public void answerListSizeZero() {
+			//given
+			//when
+			List<Answer> result = answerRepository.findAll();
+			//then
+			assertThat(result.size()).isEqualTo(0);
+		}
+
+		@Test
+		@DisplayName("갤러리 조회 실패 - 등록된 답변의 상태가 OPEN인 답변이 없음")
+		public void notExistAnswerStateOpen() {
+			//given
+			//when
+			List<Answer> result = answerRepository.findAllByState(State.OPEN);
+			//then
+			assertThat(result.size()).isEqualTo(0);
+		}
+
 	}
 
-	@Test
-	@DisplayName("갤러리 조회 실패 - 등록된 답변의 상태가 OPEN인 답변이 없음")
-	public void notExistAnswerStateOpen() {
-		//given
-		//when
-		List<Answer> result = answerRepository.findAllByState(State.OPEN);
-		//then
-		assertThat(result.size()).isEqualTo(0);
+	@Nested
+	@DisplayName("갤러리 조회 성공 테스트")
+	class GalleryReadSuccessTest{
+		@Test
+		@DisplayName("갤러리 조회 성공 ")
+		public void readGallery() {
+			//given
+			final Answer answer = Answer.builder()
+					.questionId(9)
+					.contents("답변 내용")
+					.state(State.OPEN)
+					.build();
+			final Answer answer2 = Answer.builder()
+					.questionId(9)
+					.contents("답변 내용입니다.")
+					.state(State.OPEN)
+					.build();
+			final Answer answer3 = Answer.builder()
+					.questionId(3)
+					.contents("답변 내용이지롱.")
+					.state(State.CLOSE)
+					.build();
+			answerRepository.save(answer);
+			answerRepository.save(answer2);
+			answerRepository.save(answer3);
+			//when
+			List<Answer> result = answerRepository.findAllByState(State.OPEN);
+			//then
+			assertThat(result.size()).isEqualTo(2);
+		}
 	}
 
-	@Test
-	@DisplayName("갤러리 조회 성공 ")
-	public void readGallery() {
-		//given
-		final Answer answer = Answer.builder()
-			.questionId(9)
-			.contents("답변 내용")
-			.state(State.OPEN)
-			.build();
-		final Answer answer2 = Answer.builder()
-			.questionId(9)
-			.contents("답변 내용입니다.")
-			.state(State.OPEN)
-			.build();
-		final Answer answer3 = Answer.builder()
-			.questionId(3)
-			.contents("답변 내용이지롱.")
-			.state(State.CLOSE)
-			.build();
-		answerRepository.save(answer);
-		answerRepository.save(answer2);
-		answerRepository.save(answer3);
-		//when
-		List<Answer> result = answerRepository.findAllByState(State.OPEN);
-		//then
-		assertThat(result.size()).isEqualTo(2);
-	}
+
 
 }
