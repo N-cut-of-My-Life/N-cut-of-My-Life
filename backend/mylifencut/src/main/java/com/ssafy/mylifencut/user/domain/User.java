a6ca45634896e2388f734d8122019cf1d672d830
Il kwon <alexryu1105@naver.com>
2022-11-07 10:08:43 +0900

    feat<BE>: Spring Security를 위한 UserDetails 상속 추가

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/domain/User.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/domain/User.java
@@ -1,6 +1,7 @@
 package com.ssafy.mylifencut.user.domain;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -9,6 +10,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.ssafy.mylifencut.article.domain.Article;
 import com.ssafy.mylifencut.like.domain.IsLike;
@@ -27,7 +31,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @EqualsAndHashCode
 @Table(name = "member")
-public class User {
+public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -53,5 +57,40 @@ public class User {
 			.name(userInfo.getName())
 			.email(userInfo.getEmail())
 			.build();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
 	}
 }
