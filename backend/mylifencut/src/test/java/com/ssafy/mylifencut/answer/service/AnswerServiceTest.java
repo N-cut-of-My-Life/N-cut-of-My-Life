a26e226c54bfae955d6de36833db81efde27a0a3
sungsoo <tjdtn6725@naver.com>
2022-11-07 17:47:15 +0900

    test : AnswerServiceTest 답변등록 테스트 @Nested

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/answer/service/AnswerServiceTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/answer/service/AnswerServiceTest.java
@@ -39,49 +39,59 @@ class AnswerServiceTest {
 	@Mock
 	private AnswerRepository answerRepository;
 
-	@Test
-	@DisplayName("답변등록 - 공개여부를 선택할 수 없는 답변의 상태가 OPEN일 때")
-	public void invalidState() {
-		//given
-		final Answer invalidAnswer = Answer.builder()
-			.id(0)
-			.questionId(1)
-			.contents("잘못됨")
-			.state(State.OPEN)
-			.article(Article.builder().build())
-			.build();
+	@Nested
+	@DisplayName("답변 등록 테스트")
+	class AnswerRegisterTest{
+		@Test
+		@DisplayName("답변등록 - 공개여부를 선택할 수 없는 답변의 상태가 OPEN일 때")
+		public void invalidState() {
+			//given
+			final Answer invalidAnswer = Answer.builder()
+				.id(0)
+				.questionId(1)
+				.contents("잘못됨")
+				.imgUrl("/dir/src")
+				.state(State.OPEN)
+				.article(Article.builder().build())
+				.build();
 
-		doReturn(invalidAnswer).when(answerRepository).save(any(Answer.class));
-		//when
-		final InvalidStateException result = assertThrows(InvalidStateException.class, () -> answerService.createAnswer(
-			new AnswerRegisterRequest(1, invalidAnswer.getQuestionId(), invalidAnswer.getContents(),
-				invalidAnswer.getState())));
-		//then
-		assertEquals(result.getMessage(), AnswerConstant.INVALID_STATE_ERROR_MESSAGE);
+			doReturn(invalidAnswer).when(answerRepository).save(any(Answer.class));
+			//when
+			final InvalidStateException result = assertThrows(InvalidStateException.class, () -> answerService.createAnswer(
+				new AnswerRegisterRequest(1, invalidAnswer.getQuestionId(), invalidAnswer.getContents(), invalidAnswer.getImgUrl(),
+					invalidAnswer.getState())));
+			//then
+			assertEquals(result.getMessage(), AnswerConstant.INVALID_STATE_ERROR_MESSAGE);
+		}
+
+		@Test
+		@DisplayName("답변등록 - 성공")
+		public void createAnswer() {
+			//given
+			Answer answer = newAnswer();
+			AnswerResponse answerResponse = AnswerResponse.of(answer);
+			doReturn(answer).when(answerRepository).save(any(Answer.class));
+
+			//when
+			AnswerRegisterRequest answerRegisterRequest = new AnswerRegisterRequest(0, answer.getQuestionId(),
+				answer.getContents(),
+				answer.getImgUrl(),
+				answer.getState());
+
+			final AnswerResponse result = answerService.createAnswer(answerRegisterRequest);
+
+			//then
+			assertNotNull(result);
+			assertEquals(answerResponse.getId(), result.getId());
+			assertEquals(answerResponse.getImgUrl(), result.getImgUrl());
+			assertEquals(answerResponse.getArticleId(), result.getArticleId());
+			assertEquals(answerResponse.getQuestionId(), result.getQuestionId());
+			assertEquals(answerResponse.getContents(), result.getContents());
+			assertEquals(answerResponse.getState(), result.getState());
+		}
+
 	}
 
-	@Test
-	@DisplayName("답변등록 - 성공")
-	public void createAnswer() {
-		//given
-		Answer answer = newAnswer();
-		AnswerResponse answerResponse = AnswerResponse.of(answer);
-		doReturn(answer).when(answerRepository).save(any(Answer.class));
-
-		//when
-		AnswerRegisterRequest answerRegisterRequest = new AnswerRegisterRequest(0, answer.getQuestionId(),
-			answer.getContents(),
-			answer.getState());
-		final AnswerResponse result = answerService.createAnswer(answerRegisterRequest);
-
-		//then
-		assertNotNull(result);
-		assertEquals(answerResponse.getId(), result.getId());
-		assertEquals(answerResponse.getArticleId(), result.getArticleId());
-		assertEquals(answerResponse.getQuestionId(), result.getQuestionId());
-		assertEquals(answerResponse.getContents(), result.getContents());
-		assertEquals(answerResponse.getState(), result.getState());
-	}
 
 
 	@Nested
