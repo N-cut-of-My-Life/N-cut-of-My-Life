0d5afb108399367dacc1049846ef24406d6a070a
choijoohee <choijoohee@naver.com>
2022-11-07 11:50:57 +0900

    feat<BE>: 여행일지 등록 controller 프로덕션 코드 작성

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/article/controller/ArticleController.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/article/controller/ArticleController.java
@@ -6,9 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.mylifencut.article.dto.ArticleRequest;
 import com.ssafy.mylifencut.article.service.ArticleService;
 import com.ssafy.mylifencut.common.dto.BaseResponse;
 
@@ -27,5 +30,14 @@ public class ArticleController {
 			RETRIEVE_SUCCESS_MESSAGE,
 			articleService.retrieveArticles(userId))
 			, HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity<BaseResponse> registerArticle(@RequestBody final ArticleRequest articleRequest) {
+		articleService.createArticle(articleRequest);
+		return new ResponseEntity<>(BaseResponse.from(
+			true,
+			REGISTER_SUCCESS_MESSAGE
+		), HttpStatus.OK);
 	}
 }
