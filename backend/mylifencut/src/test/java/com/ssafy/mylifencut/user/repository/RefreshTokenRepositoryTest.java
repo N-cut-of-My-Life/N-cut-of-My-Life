0420efa6e9ec7efdea9cb262086a911925c2d6bc
Il kwon <alexryu1105@naver.com>
2022-11-08 10:10:58 +0900

    test<BE>: RefreshTokenRepository 저장 테스트 추가

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/repository/RefreshTokenRepositoryTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/repository/RefreshTokenRepositoryTest.java
@@ -9,14 +9,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import com.ssafy.mylifencut.user.domain.RefreshToken;
+
 @DataJpaTest
+@DisplayName("리프레쉬 토큰 레포 테스트")
 class RefreshTokenRepositoryTest {
 	@Autowired
 	private RefreshTokenRepository refreshTokenRepository;
 
 	@DisplayName("리프레쉬 토큰으로 UserId 검색")
 	@Test
-	public void findUserIdByRefreshToken() {
+	public void findUserIdByRefreshTokenFail() {
 		// given
 		String refreshToken = "REFRESH_TOKEN";
 
@@ -25,5 +28,22 @@ class RefreshTokenRepositoryTest {
 
 		// then
 		assertFalse(userId.isPresent());
+	}
+
+	@DisplayName("리프레쉬 토큰 저장")
+	@Test
+	public void findUserIdByRefreshTokenSuccess() {
+		// given
+		RefreshToken refreshToken = RefreshToken.builder()
+			.userId(1)
+			.token("TOKEN")
+			.build();
+
+		// when
+		final RefreshToken result = refreshTokenRepository.save(refreshToken);
+
+		// then
+		assertEquals(refreshToken.getUserId(), refreshToken.getUserId());
+		assertEquals(refreshToken.getToken(), refreshToken.getToken());
 	}
 }
