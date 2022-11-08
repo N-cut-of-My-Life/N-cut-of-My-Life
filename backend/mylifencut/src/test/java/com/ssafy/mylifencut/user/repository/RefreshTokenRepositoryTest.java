c4e710949fa868f74cb50b92f78b361c04a21803
Il kwon <alexryu1105@naver.com>
2022-11-08 10:04:05 +0900

    test<BE>: RefreshTokenRepository 리프레쉬 토큰으로 UserId 검색 테스트 추가

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/repository/RefreshTokenRepositoryTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/repository/RefreshTokenRepositoryTest.java
@@ -1,5 +1,11 @@
 package com.ssafy.mylifencut.user.repository;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -7,4 +13,17 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 class RefreshTokenRepositoryTest {
 	@Autowired
 	private RefreshTokenRepository refreshTokenRepository;
+
+	@DisplayName("리프레쉬 토큰으로 UserId 검색")
+	@Test
+	public void findUserIdByRefreshToken() {
+		// given
+		String refreshToken = "REFRESH_TOKEN";
+
+		// when
+		final Optional<Integer> userId = refreshTokenRepository.findByRefreshToken();
+
+		// then
+		assertFalse(userId.isPresent());
+	}
 }
