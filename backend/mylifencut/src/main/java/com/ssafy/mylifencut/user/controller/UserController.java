00b5c138d9c5030f31fb314a3d395738da4459f8
Il kwon <alexryu1105@naver.com>
2022-11-08 09:21:43 +0900

    fix<BE>: RequestBody Map으로 수정

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/controller/UserController.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.ssafy.mylifencut.user.controller;
 
 import static com.ssafy.mylifencut.user.UserConstant.*;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import com.ssafy.mylifencut.common.dto.BaseResponse;
 import com.ssafy.mylifencut.user.JwtTokenProvider;
 import com.ssafy.mylifencut.user.service.UserService;
@@ -26,9 +26,8 @@ public class UserController {
 	private final JwtTokenProvider jwtTokenProvider;
 
 	@PostMapping("/login")
-	public ResponseEntity<BaseResponse> kakaoLogin(@RequestBody String accessToken) {
-		JsonElement element = JsonParser.parseString(accessToken);
-		accessToken = element.getAsJsonObject().get("accessToken").getAsString();
+	public ResponseEntity<BaseResponse> kakaoLogin(@RequestBody Map<String, String> json) {
+		String accessToken = json.get("accessToken");
 		String jwtToken = jwtTokenProvider.createToken(Integer.toString(userService.kakaoLogin(accessToken)));
 		return new ResponseEntity<>(BaseResponse.from(true, KAKAO_LOGIN_SUCCESS_MESSAGE, jwtToken), HttpStatus.OK);
 	}
