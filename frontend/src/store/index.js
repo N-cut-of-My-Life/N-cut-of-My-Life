fbdb4738230869e9f73d926e74644382ac1a1b9c
DESKTOP-KVCQHCD\SSAFY <niinp@naver.com>
2022-11-10 13:18:36 +0900

    feat<FE>: 음악 검색 함수 로직 구현

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -30,7 +30,7 @@ export const useGalleryStore = defineStore('gallery', {
 export const useMusicStore = defineStore('music', {
   state: () => ({
       isMute: false, 
-      items: ''
+      songs: [],
     }),
   getters: {
   },
@@ -40,16 +40,39 @@ export const useMusicStore = defineStore('music', {
       myAudio.muted = !myAudio.muted;
     },
     
-    getMusicData() {
+    getMusicData(keyword) {
       axios({
-        url: 'https://www.maniadb.com/api/search/기다리다/?sr=song&display=10&key=niinp28@gmail.com&v=0.5',
+        url: `https://www.maniadb.com/api/search/${keyword}/?sr=song&display=5&key=niinp28@gmail.com&v=0.5`,
         method: 'GET'
       })
       .then((res) => {
         
-        const json = xml2json(res.data)
-        this.items = json
-        
+        const json_string = xml2json(res.data)
+        const json = JSON.parse(json_string)
+        let lst = (json.elements[0].elements[0].elements).slice(8)
+        let songsList = new Array()
+        // 가수, 제목, 앨범표지 추출
+        for(let e1 of lst){
+          let song = new Object()
+          for(let e2 of e1.elements){
+            if(e2.name === 'title'){
+              console.log(e2.elements[0].cdata)
+              song['title'] = e2.elements[0].cdata
+            }
+            if(e2.name === 'maniadb:album'){
+              console.log(e2.elements[3].elements[0].cdata)
+              song['image'] = e2.elements[3].elements[0].cdata
+            }
+            if(e2.name ==='maniadb:artist'){
+              console.log(e2.elements[1].elements[0].cdata)
+              song['artist'] = e2.elements[1].elements[0].cdata
+            }
+          }
+          if(song !== {}){
+            songsList.push(song)
+          }
+        }
+        this.songs = songsList
       })
     }
     
