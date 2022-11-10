75b21f9eff88967d61efd8bae99d8e3b5b53c7a4
Il kwon <alexryu1105@naver.com>
2022-11-10 12:38:05 +0900

    test<BE>: UserTest [성공] - 비밀번호 받기 테스트 작성

--- backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/domain/UserTest.java
+++ backend/mylifencut/src/test/java/com/ssafy/mylifencut/user/domain/UserTest.java
@@ -1,8 +1,24 @@
 package com.ssafy.mylifencut.user.domain;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("[유저 도메인]")
 class UserTest {
+
+	@Test
+	@DisplayName("[성공] - 비밀번호 받기")
+	void getPassword() {
+		// given
+		final User user = User.builder().build();
+
+		// when
+		String result = user.getPassword();
+
+		// then
+		assertNull(result);
+	}
 
 }
