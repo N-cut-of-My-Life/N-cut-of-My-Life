2b35d8282c03ee0b3a14371f965f5a4b71685148
Il kwon <alexryu1105@naver.com>
2022-11-11 13:02:09 +0900

    feat<BE>: RefreshToken Cookie에 추가

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/controller/UserController.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/controller/UserController.java
@@ -2,8 +2,13 @@ package com.ssafy.mylifencut.user.controller;
 
 import static com.ssafy.mylifencut.user.UserConstant.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.mylifencut.common.dto.BaseResponse;
 import com.ssafy.mylifencut.user.dto.KakaoRequest;
 import com.ssafy.mylifencut.user.dto.Token;
-import com.ssafy.mylifencut.user.dto.TokenRequest;
 import com.ssafy.mylifencut.user.service.UserService;
 
 import io.swagger.annotations.ApiOperation;
@@ -25,6 +29,8 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/user")
 public class UserController {
 
+	private static final int COOKIE_EXPIRE_TIME = 60 * 60 * 24 * 14;    // 쿠키 14일 유지
+
 	private final UserService userService;
 
 	@PostMapping("/login")
@@ -35,15 +41,28 @@ public class UserController {
 			BaseResponse.from(true, KAKAO_LOGIN_SUCCESS_MESSAGE, userService.kakaoLogin(accessToken)), HttpStatus.OK);
 	}
 
-	@PostMapping("/token")
+	@GetMapping("/token")
 	@ApiOperation(value = "Token 갱신", notes = "Refresh, Access Token을 보내서 새로 갱신된 토큰 발급")
 	@ApiResponses(value = {
 		@ApiResponse(code = 200, message = "토큰 재발급 성공", response = Token.class),
 		@ApiResponse(code = 400, message = "토큰 오류로 재발급 실패")
 	})
-	public ResponseEntity<BaseResponse> reissueToken(@RequestBody TokenRequest tokenRequest) {
+	public ResponseEntity<BaseResponse> reissueToken(
+		@CookieValue(value = "refreshToken", required = false) String refreshToken, HttpServletResponse response) {
+		Token token = userService.reissueToken(refreshToken);
+
+		Cookie cookie = new Cookie("refreshToken", token.getRefreshToken());
+
+		cookie.setSecure(true);
+		cookie.setHttpOnly(true);
+		cookie.setPath("/");
+
+		cookie.setMaxAge(COOKIE_EXPIRE_TIME);
+
+		response.addCookie(cookie);
+
 		return new ResponseEntity<>(
-			BaseResponse.from(true, TOKEN_REISSUE_SUCCESS_MESSAGE, userService.reissueToken(tokenRequest)),
+			BaseResponse.from(true, TOKEN_REISSUE_SUCCESS_MESSAGE, token.getAccessToken()),
 			HttpStatus.OK);
 	}
 }
