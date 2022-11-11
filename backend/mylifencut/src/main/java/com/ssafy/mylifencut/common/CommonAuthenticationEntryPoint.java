80c0ba9ed46e76e815e3664c8a8e1b4b449bbbaa
Il kwon <alexryu1105@naver.com>
2022-11-11 11:06:53 +0900

    feat<BE>: JWT 없이 요청 보내는 경우 Custom 예외 처리 추가

---
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/common/CommonAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.ssafy.mylifencut.common;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException authException) throws IOException, ServletException {
+		System.out.println("EXCEPTION");
+		response.sendRedirect("/user/exception");
+	}
+}
