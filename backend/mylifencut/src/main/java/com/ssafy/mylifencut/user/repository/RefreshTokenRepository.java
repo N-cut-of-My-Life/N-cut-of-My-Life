0f74698e4dd8d142838568d56dc98375d52f66bc
Il Kwon <alexryu1105@naver.com>
2022-11-12 15:09:16 +0900

    feat<BE>: RefreshTokenRepository 함수 추가

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/repository/RefreshTokenRepository.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/user/repository/RefreshTokenRepository.java
@@ -1,13 +1,12 @@
 package com.ssafy.mylifencut.user.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ssafy.mylifencut.user.domain.RefreshToken;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
 	Optional<RefreshToken> findByToken(String token);
 
-	Optional<RefreshToken> findByUserId(Integer id);
+	List<RefreshToken> findAllByUserId(Integer id);
 }
