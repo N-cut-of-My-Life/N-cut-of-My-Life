5d412d8baebd4d8155ad56fb3323a2d6c292902e
JeongGeon <scvscv22@hanmail.net>
2022-11-15 16:03:19 +0900

    Feat<FE>:결과출력페이지틀완성

---
+++ frontend/src/components/result/PortraitForm.vue
@@ -0,0 +1,618 @@
+<template>
+  <div
+    data-aos="fade-right"
+    data-aos-offset="50"
+    data-aos-easing="ease-in-sine"
+    class="timeline haha"
+  >
+    <ul>
+      <li>
+        <span>하하호호행성</span>
+        <div class="content">
+          <h3>가장 행복했던 때가 언제였는지 적어줘!</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div
+    data-aos="fade-left"
+    data-aos-offset="50"
+    data-aos-easing="ease-in-sine"
+    class="timeline precious"
+  >
+    <ul>
+      <li>
+        <span>넌너무소중행성</span>
+        <div class="content">
+          <h3>너에게 가장 소중한 사람은 누구니?</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div
+    data-aos="fade-right"
+    data-aos-offset="50"
+    data-aos-easing="ease-in-sine"
+    class="timeline sad"
+  >
+    <ul>
+      <li>
+        <span>훌쩍훌쩍행성 </span>
+        <div class="content">
+          <h3>너의 가장 슬펐던 기억을 적어주겠니?</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div
+    data-aos="fade-left"
+    data-aos-offset="50"
+    data-aos-easing="ease-in-sine"
+    class="timeline treasure"
+  >
+    <ul>
+      <li>
+        <span>반짝반짝행성 </span>
+        <div class="content">
+          <h3>가장 소중히 여기는 물건이 뭔지 적어주겠니?</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div
+    data-aos="fade-right"
+    data-aos-offset="50"
+    data-aos-easing="ease-in-sine"
+    class="timeline dream"
+  >
+    <ul>
+      <li>
+        <span>이루지못행성 </span>
+        <div class="content">
+          <h3>이루지 못한꿈은 무엇이 있니? 잠시 맡겨두어도 좋단다!</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div
+    data-aos="fade-left"
+    data-aos-offset="50"
+    data-aos-easing="ease-in-sine"
+    class="timeline regret"
+  >
+    <ul>
+      <li>
+        <span>괜히글행성 </span>
+        <div class="content">
+          <h3>후회되는 일은 여기 있는 후회쓰레기통에 버리고 가렴</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div
+    data-aos="fade-right"
+    data-aos-offset="50"
+    data-aos-easing="ease-in-sine"
+    class="timeline dumchit"
+  >
+    <ul>
+      <li>
+        <span>둠칫둠칫행성</span>
+        <div class="content">
+          <h3>네가 가장 좋아하는 음악은 무엇이니?</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div
+    data-aos="fade-left"
+    data-aos-offset="50"
+    data-aos-easing="ease-in-sine"
+    class="timeline genie"
+  >
+    <ul>
+      <li>
+        <span>지니행성</span>
+        <div class="content">
+          <h3>너의 소원은 무엇이니? 무엇이든 좋으니 적어볼래?</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div
+    data-aos="fade-down"
+    data-aos-offset="50"
+    data-aos-easing="ease-in-sine"
+    class="timeline lastword"
+  >
+    <ul>
+      <li>
+        <span>나에게 하고 싶은 한마디</span>
+        <div class="content">
+          <h3>너에게 남기고 싶은 말이 있니?</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.timeline {
+  width: 100vw;
+  height: 80vh;
+  margin: -0.1rem;
+  overflow: hidden;
+}
+.haha {
+  background-image: url("@/assets/PlanetBackground/haha.svg");
+  background-position: left bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-color: rgba(255, 255, 255, 255);
+}
+.haha ul li span {
+  display: inline-block;
+  background-color: #ce49bf;
+  color: #feb139;
+  border-radius: 1.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 1rem;
+  text-align: center;
+}
+.haha ul li .content h3 {
+  color: #ce49bf;
+  font-size: 1.25rem;
+  padding-top: 0.5rem;
+}
+.haha ul li .content p {
+  width: 50vw;
+  height: 80vh;
+  padding: 0;
+  font-size: 1rem;
+  color: white;
+  opacity: 0;
+}
+.haha ul li:hover {
+  background-color: #feb139;
+}
+.precious {
+  background-image: url("@/assets/PlanetBackground/precious.svg");
+  background-position: left bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-color: rgba(255, 255, 255, 255);
+}
+.precious ul li span {
+  display: inline-block;
+  background-color: #ffed99;
+  color: #f6b8b8;
+  border-radius: 1.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 1rem;
+  text-align: center;
+}
+.precious ul li .content h3 {
+  color: #ffed99;
+  font-size: 1.25rem;
+  padding-top: 0.5rem;
+}
+.precious ul li .content p {
+  width: auto;
+  height: 80vh;
+  padding: 0;
+  font-size: 1rem;
+  color: white;
+  opacity: 0;
+}
+.precious ul li:hover {
+  background-color: #f6b8b8;
+}
+.sad {
+  background-image: url("@/assets/PlanetBackground/sad_2.svg");
+  background-position: right bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-color: rgba(255, 255, 255, 255);
+}
+.sad ul li span {
+  display: inline-block;
+  background-color: #ffc947;
+  color: #0a1931;
+  border-radius: 1.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 1rem;
+  text-align: center;
+}
+.sad ul li .content h3 {
+  color: #ffc947;
+  font-size: 1.25rem;
+  padding-top: 0.5rem;
+}
+.sad ul li .content p {
+  width: 50vw;
+  height: 80vh;
+  padding: 0;
+  font-size: 1rem;
+  color: white;
+  opacity: 0;
+}
+.sad ul li:hover {
+  background-color: #0a1931;
+}
+.treasure {
+  background-image: url("@/assets/PlanetBackground/treasure.svg");
+  background-position: left bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-color: rgba(255, 255, 255, 255);
+}
+.treasure ul li span {
+  display: inline-block;
+  background-color: #ffefcf;
+  color: #ff7a00;
+  border-radius: 1.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 1rem;
+  text-align: center;
+}
+.treasure ul li .content h3 {
+  color: #ffefcf;
+  font-size: 1.25rem;
+  padding-top: 0.5rem;
+}
+.treasure ul li .content p {
+  width: 50vw;
+  height: 80vh;
+  padding: 0;
+  font-size: 1rem;
+  color: white;
+  opacity: 0;
+}
+.treasure ul li:hover {
+  background-color: #ff7a00;
+}
+.dream {
+  background-image: url("@/assets/PlanetBackground/dream_2.svg");
+  background-position: right bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-color: rgba(255, 255, 255, 255);
+}
+.dream ul li span {
+  display: inline-block;
+  background-color: #fadcaa;
+  color: #c56183;
+  border-radius: 1.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 1rem;
+  text-align: center;
+}
+.dream ul li .content h3 {
+  color: #fadcaa;
+  font-size: 1.25rem;
+  padding-top: 0.5rem;
+}
+.dream ul li .content p {
+  width: 50vw;
+  height: 80vh;
+  padding: 0;
+  font-size: 1rem;
+  color: #fb7b6b;
+  opacity: 0;
+}
+.dream ul li:hover {
+  background-color: #c56183;
+}
+.regret {
+  background-image: url("@/assets/PlanetBackground/regret.svg");
+  background-position: left bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-color: rgba(255, 255, 255, 255);
+}
+.regret ul li span {
+  display: inline-block;
+  background-color: #f2e7d5;
+  color: #6d9886;
+  border-radius: 1.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 1rem;
+  text-align: center;
+}
+.regret ul li .content h3 {
+  color: #f2e7d5;
+  font-size: 1.25rem;
+  padding-top: 0.5rem;
+}
+.regret ul li .content p {
+  width: 50vw;
+  height: 80vh;
+  padding: 0;
+  font-size: 1rem;
+  color: white;
+  opacity: 0;
+}
+.regret ul li:hover {
+  background-color: #6d9886;
+}
+.dumchit {
+  background-image: url("@/assets/PlanetBackground/dumchit_2.svg");
+  background-position: right bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-color: rgba(255, 255, 255, 255);
+}
+.dumchit ul li span {
+  display: inline-block;
+  background-color: #647e68;
+  color: #f5c7a9;
+  border-radius: 1.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 1rem;
+  text-align: center;
+}
+.dumchit ul li .content h3 {
+  color: #647e68;
+  font-size: 1.25rem;
+  padding-top: 0.5rem;
+}
+.dumchit ul li .content p {
+  width: 50vw;
+  height: 80vh;
+  padding: 0;
+  font-size: 1rem;
+  color: white;
+  opacity: 0;
+}
+.dumchit ul li:hover {
+  background-color: #f5c7a9;
+}
+.genie {
+  background-image: url("@/assets/PlanetBackground/genie.svg");
+  background-position: left bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-color: rgba(255, 255, 255, 255);
+}
+.genie ul li span {
+  display: inline-block;
+  background-color: #fce2db;
+  color: #b270a2;
+  border-radius: 1.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 1rem;
+  text-align: center;
+}
+.genie ul li .content h3 {
+  color: #fce2db;
+  font-size: 1.25rem;
+  padding-top: 0.5rem;
+}
+.genie ul li .content p {
+  width: 50vw;
+  height: 80vh;
+  padding: 0;
+  font-size: 1rem;
+  color: white;
+  opacity: 0;
+}
+.genie ul li:hover {
+  background-color: #b270a2;
+}
+.lastword {
+  background-image: url("@/assets/PlanetBackground/lastword.png");
+  background-position: right bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-color: rgba(255, 255, 255, 255);
+}
+.lastword ul li span {
+  display: inline-block;
+  background-color: #ef5b0c;
+  color: #d4f6cc;
+  border-radius: 1.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 1rem;
+  text-align: center;
+}
+.lastword ul li .content h3 {
+  color: #ef5b0c;
+  font-size: 1.25rem;
+  padding-top: 0.5rem;
+}
+.lastword ul li .content p {
+  width: 50vw;
+  height: 80vh;
+  padding: 0;
+  font-size: 1rem;
+  color: #100f0f;
+  opacity: 0;
+}
+.lastword ul li:hover {
+  background-color: #d4f6cc;
+}
+
+.timeline ul {
+  list-style-type: none;
+  border-left: 0.25rem solid #094a68;
+  padding: 6rem 3rem 6rem 0.25rem;
+  margin-left: 20rem;
+  margin-right: 20rem;
+  height: 100vh;
+}
+.timeline ul li {
+  padding: 1.5rem 1.5rem;
+  position: relative;
+  cursor: pointer;
+  transition: 0.5s;
+}
+.timeline ul li .content {
+  height: 40vh;
+  display: absolute;
+  overflow: hidden;
+}
+
+.timeline ul li .content:hover p {
+  opacity: 1;
+}
+.timeline ul li:before {
+  position: absolute;
+  content: "";
+  width: 1rem;
+  height: 1rem;
+  background-color: #34ace0;
+  border-radius: 50%;
+  left: -0.9rem;
+  top: 1.75rem;
+  transition: 0.5s;
+}
+.timeline ul li:hover:before {
+  background-color: #0f0;
+  box-shadow: 0 0 0.75rem 0.125rem #0f0;
+}
+@media (max-width: 300px) {
+  .timeline {
+    width: 100%;
+    padding: 0;
+  }
+  .timeline ul li .content h3 {
+    color: #34ace0;
+    font-size: 1rem;
+  }
+}
+
+/* .timeline ul li span {
+      display: inline-block;
+      background-color: #38e54d;
+      border-radius: 1.5rem;
+      padding: 0.25rem 0.5rem;
+      font-size: 1rem;
+      text-align: center;
+    }
+    .timeline ul li .content h3 {
+      color: #38e54d;
+      font-size: 1.25rem;
+      padding-top: 0.5rem;
+    }
+    .timeline ul li .content p {
+      width: 50vw;
+      height: 80vh;
+      padding: 0;
+      font-size: 1rem;
+      color: white;
+      opacity: 0;
+    } */
+/* .timeline ul li:hover {
+      background-color: #071f2a;
+      } */
+/* .timeline:nth-child(2) {
+      background-image: url("@/assets/PlanetBackground/haha.svg");
+      background-position: left bottom;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      background-color: rgba(255, 255, 255, 255);
+    }
+    .timeline:nth-child(3) {
+      background-image: url("@/assets/PlanetBackground/precious.svg");
+      background-position: left bottom;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      background-color: rgba(255, 255, 255, 255);
+    }
+    .timeline:nth-child(4) {
+      background-image: url("@/assets/PlanetBackground/sad_2.svg");
+      background-position: right bottom;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      background-color: rgba(255, 255, 255, 255);
+    }
+    .timeline:nth-child(5) {
+      background-image: url("@/assets/PlanetBackground/treasure.svg");
+      background-position: left bottom;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      background-color: rgba(255, 255, 255, 255);
+    }
+    .timeline:nth-child(6) {
+      background-image: url("@/assets/PlanetBackground/dream_2.svg");
+      background-position: right bottom;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      background-color: rgba(255, 255, 255, 255);
+    }
+    .timeline:nth-child(7) {
+      background-image: url("@/assets/PlanetBackground/regret.svg");
+      background-position: left bottom;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      background-color: rgba(255, 255, 255, 255);
+    }
+    .timeline:nth-child(8) {
+      background-image: url("@/assets/PlanetBackground/dumchit_2.svg");
+      background-position: right bottom;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      background-color: rgba(255, 255, 255, 255);
+    }
+    .timeline:nth-child(9) {
+      background-image: url("@/assets/PlanetBackground/genie.svg");
+      background-position: left bottom;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      background-color: rgba(255, 255, 255, 255);
+    }
+    .timeline:nth-child(9) {
+      background-image: url("@/assets/PlanetBackground/sad_2.svg");
+      background-position: right bottom;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      background-color: rgba(255, 255, 255, 255);
+    } */
+</style>
--- frontend/src/views/result/ResultPrint.vue
+++ frontend/src/views/result/ResultPrint.vue
@@ -1,171 +1,51 @@
 <template>
-  <div ref="pdfarea">
+  <button class="button-go-back" @click="goBack">〈</button>
+  <button class="button-go-home" @click="goHome">Intro</button>
+  <div ref="pdfarea" class="wrapper">
+    <landscape-form></landscape-form>
+    <portrait-form></portrait-form>
     <audio id="myaudios" loop autoplay volume="0.2">
       <source src="@/assets/audio/out-of-time.mp3" type="audio/mp3" />
     </audio>
-    <section class="result-print">
-      <button id="pdf-button-area" @click="exportToPDF">다운로드</button>
-      <ol>
-        <li>
-          <div style="left: 0rem">
-            <p class="ttl">하하호호행성</p>
-            <p class="ctnt">가장 행복했던 때가<br />언제였는지 적어줘!</p>
-          </div>
-          <img
-            class="planet"
-            src="@/assets/planet/haho.png"
-            style="
-              width: 12rem;
-              height: 12rem;
-              border-radius: 50%;
-              transform: scale(0.65);
-            "
-          />
-        </li>
-        <li>
-          <div>
-            <p class="ttl">넌너무소중행성</p>
-            <p class="ctnt">
-              너에게<br />
-              가장 소중한 사람은 누구니?
-            </p>
-          </div>
-          <img
-            class="planet"
-            src="@/assets/planet/dearyou.png"
-            style="
-              width: 12rem;
-              height: 12rem;
-              border-radius: 50%;
-              transform: scale(0.8);
-            "
-          />
-        </li>
-        <li>
-          <div>
-            <p class="ttl">훌쩍훌쩍행성</p>
-            <p class="ctnt">
-              너의 가장 슬펐던 기억을<br />
-              적어주겠니?
-            </p>
-          </div>
-          <img
-            class="planet"
-            src="@/assets/planet/weep.png"
-            style="
-              width: 12rem;
-              height: 12rem;
-              border-radius: 50%;
-              transform: scale(1);
-            "
-          />
-        </li>
-        <li>
-          <div>
-            <p class="ttl">반짝반짝행성</p>
-            <p class="ctnt">
-              가장 소중히 여기는 물건이<br />
-              뭔지 적어주겠니?
-            </p>
-          </div>
-          <img class="planet" src="@/assets/planet/blink.png" />
-        </li>
-        <li>
-          <div>
-            <p class="ttl">이루지못행성</p>
-            <p class="ctnt">
-              이루지 못한꿈은 무엇이 있니?<br />
-              잠시 맡겨두어도 좋단다!
-            </p>
-          </div>
-          <img
-            class="planet"
-            src="@/assets/planet/didnt.png"
-            style="
-              width: 12rem;
-              height: 12rem;
-              border-radius: 50%;
-              transform: scale(0.5);
-            "
-          />
-        </li>
-        <li>
-          <div>
-            <p class="ttl">괜히글행성</p>
-            <p class="ctnt">
-              후회되는 일은 여기 있는<br />
-              후회쓰레기통에 버리고 가렴
-            </p>
-          </div>
-          <img
-            class="planet"
-            src="@/assets/planet/forget.png"
-            style="
-              width: 12rem;
-              height: 12rem;
-              border-radius: 50%;
-              transform: scale(0.7);
-            "
-          />
-        </li>
-        <li>
-          <div>
-            <p class="ttl">둠칫둠칫행성</p>
-            <p class="ctnt">
-              네가 가장 좋아하는 음악은<br />
-              무엇이니?
-            </p>
-          </div>
-          <img
-            class="planet"
-            src="@\assets\planet\music.png"
-            style="width: 12rem; height: 12rem"
-          />
-        </li>
-        <li>
-          <div class="genie" style="left: -1rem; top: 17rem; z-index: 22">
-            <p class="ttl">지니행성</p>
-            <p class="ctnt">
-              너의 소원은 무엇이니?<br />무엇이든 좋으니 적어볼래?
-            </p>
-          </div>
-          <img
-            src="@/assets/space.svg"
-            style="position: absolute; top: -10rem"
-          />
-          <img
-            src="@/assets/genie.svg"
-            style="position: absolute; top: 10rem; left: 4rem"
-          />
-        </li>
-      </ol>
-      <div class="kakao-sharing-btn">
-        <button id="kakaotalk-sharing-result-btn" @click="kakaoShare">
-          <img
-            src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
-            alt="카카오톡 공유 보내기 버튼"
-          />
-          여행일지 카카오톡 공유하기
-        </button>
-        <button id="kakaotalk-sharing-url-btn" @click="kakaoShare">
-          <img
-            src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
-            alt="카카오톡 공유 보내기 버튼"
-          />
-          친구에게도 인생N컷 추천하기
-        </button>
-      </div>
-    </section>
+    <button id="pdf-button-area" @click="exportToPDF">다운로드</button>
+  </div>
+  <div class="kakao-sharing-btn">
+    <button id="kakaotalk-sharing-result-btn" @click="kakaoShare">
+      <img
+        src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
+        alt="카카오톡 공유 보내기 버튼"
+      />
+      여행일지 카카오톡 공유하기
+    </button>
+    <button id="kakaotalk-sharing-url-btn" @click="kakaoShare">
+      <img
+        src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
+        alt="카카오톡 공유 보내기 버튼"
+      />
+      친구에게도 인생N컷 추천하기
+    </button>
   </div>
 </template>
 
 <script>
+import LandscapeForm from "@/components/result/LandscapeForm.vue";
+import PortraitForm from "@/components/result/PortraitForm.vue";
 import html2pdf from "html2pdf.js";
 import { useMusicStore } from "@/store/music";
 
 export default {
   name: "submission-detail",
+  components: {
+    LandscapeForm,
+    PortraitForm,
+  },
   methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+    goHome() {
+      this.$router.push("/");
+    },
     kakaoShare() {
       window.Kakao.Share.sendScrap({
         requestUrl: "https://k7b105.p.ssafy.io/",
@@ -203,136 +83,44 @@ export default {
 };
 </script>
 <style scoped>
-.genie::before {
-  border-block-width: 0px !important;
-}
-
-.planet {
-  position: absolute;
-}
-
-.result-print ol li:nth-child(odd) img {
-  top: 10vh;
-  left: 50%;
-}
-
-.result-print ol li:nth-child(even) img {
-  left: 50%;
-  bottom: 10vh;
-}
-
-.result-print {
-  display: -webkit-flex;
-  display: flex;
-  height: 100vh;
-  white-space: nowrap;
-  overflow: hidden;
-  background: black;
-  -webkit-align-items: center;
-  align-items: center;
-  -webkit-justify-content: center;
-  justify-content: center;
-}
-.result-print ol {
-  -webkit-flex: 1;
-  flex: 1;
-  font-size: 0;
-  width: 100vw;
-  padding: 15rem 0;
-  transition: all 1s;
-}
-.result-print ol li {
-  position: relative;
-  display: inline-block;
-  list-style-type: none;
-  width: 11.2%;
-  height: 0.5rem;
-  background: #fff;
-}
-.result-print ol li:first-child {
-  width: 8%;
-}
-.result-print ol li:last-child {
-  width: 12%;
-}
-.result-print ol li:not(:first-child) {
-  margin-left: 1rem;
-}
-.result-print ol li:not(:last-child)::after {
-  content: "";
-  position: absolute;
-  top: 50%;
-  left: calc(100% - 0.5rem);
-  bottom: 0;
-  width: 2rem;
-  height: 2rem;
-  transform: translateY(-50%);
-  border-radius: 50%;
-  background: linear-gradient(to right, #f3f3f3, #b5b5b5);
-  z-index: 20;
-}
-.result-print ol li div {
-  position: absolute;
-  left: calc(50% - 2.5rem);
-  width: 16rem;
-  padding: 1rem;
-  padding-left: 5rem;
-  font-size: 1rem;
-  white-space: nowrap;
-  color: white;
+.button-go-back {
+  position: fixed;
+  border: 0.5px solid white;
   background: transparent;
+  color: white;
+  border-radius: 10%;
+  margin: 28px 8px;
+  padding: 0px 10px 2px;
+  font-size: 20px;
+  font-family: Exo;
+  z-index: 100;
 }
-
-.result-print ol li:first-child div::before {
-  content: "";
+.button-go-back:hover {
+  background-color: white;
+  color: #141414;
+}
+.button-go-home {
   position: absolute;
-  top: 100%;
-  left: 8.2rem;
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-block-width: 4rem;
+  top: 88%;
+  border: 0.5px solid white;
+  background: transparent;
+  color: white;
+  border-radius: 10%;
+  margin: 28px 8px;
+  padding: 6px 40px 8px;
+  font-size: 20px;
+  font-family: Exo;
 }
-
-.result-print ol li:not(:first-child) div::before {
-  content: "";
-  position: absolute;
-  top: 100%;
-  left: 8.2rem;
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-block-width: 4rem;
-}
-
-.result-print ol li:nth-child(odd) div {
-  top: -13.5rem;
-  transform: translateY(-100%);
-}
-
-.result-print ol li:nth-child(odd) div::before {
-  top: 120%;
-}
-
-.result-print ol li:nth-child(even) div {
-  top: calc(100% + 16rem);
-}
-
-.result-print ol li:nth-child(even) div::before {
-  top: -144%;
-}
-.ttl {
-  font-size: 1.2rem;
-  font-weight: bold;
-  margin-bottom: 0.5rem;
-  text-indent: 0.1rem;
-}
-.ctnt {
-  overflow-wrap: normal;
+.button-go-home:hover {
+  background-color: white;
+  color: #141414;
 }
 .kakao-sharing-btn {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.wrapper {
+  overflow-x: hidden;
 }
 </style>
