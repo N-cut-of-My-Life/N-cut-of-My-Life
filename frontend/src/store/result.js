2713a78a472673ebd5734ee1a5ba2318db0754cd
choijoohee <choijoohee@naver.com>
2022-11-16 14:02:42 +0900

    feat<FE>: 결과 이미지로 저장 및 pdf로 저장

--- frontend/src/store/result.js
+++ frontend/src/store/result.js
@@ -1,8 +1,17 @@
 import { defineStore } from "pinia";
+import axios from "axios";
 
 export const useResultStore = defineStore("result", {
   state: () => ({
     from: "",
     resultArticle: {},
+    imageFile: null,
   }),
+  actions: {
+    async downloadResultImage(imageUrl) {
+      await axios.get(imageUrl, { responseType: "blob" }).then((response) => {
+        this.imageFile = response.data;
+      });
+    },
+  },
 });
