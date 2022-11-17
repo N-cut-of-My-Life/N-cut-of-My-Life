244e8197cafc4047b95be6599a92138b16640b76
sungsoo <tjdtn6725@naver.com>
2022-11-17 12:19:18 +0900

    feat : Util Class KeyWordConverterToURI 불필요한 생성자 생성을 막기 위한 @NoArgsConstructor 어노테이션 추가 및 AccessLevel.PRIVATE 옵션 추가

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/util/KeyWordConverterToURI.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/util/KeyWordConverterToURI.java
@@ -1,5 +1,9 @@
 package com.ssafy.mylifencut.answer.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class KeyWordConverterToURI {
 	public static String converter(String keyword) {
 		return "http://www.maniadb.com/api/search/" + keyword + "/?sr=song&display=10&key=example&v=0.5";
