5699d57df98404a084cf05f233df1364bdf712e3
sungsoo <tjdtn6725@naver.com>
2022-11-17 11:05:52 +0900

    feat : AnswerService 음악 조회 기능을 위한 searchMusic Service , getTagValue 메소드 구현

--- backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/service/AnswerService.java
+++ backend/mylifencut/src/main/java/com/ssafy/mylifencut/answer/service/AnswerService.java
@@ -1,14 +1,32 @@
 package com.ssafy.mylifencut.answer.service;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import com.ssafy.mylifencut.answer.domain.State;
 import com.ssafy.mylifencut.answer.dto.GalleryResponse;
+import com.ssafy.mylifencut.answer.dto.MusicResponse;
 import com.ssafy.mylifencut.answer.repository.AnswerRepository;
 import com.ssafy.mylifencut.like.domain.IsLike;
 import com.ssafy.mylifencut.like.repository.LikeRepository;
@@ -38,4 +56,39 @@ public class AnswerService {
 		return galleryResponses;
 	}
 
+	public List<MusicResponse> searchMusic(String URI) throws Exception {
+		List<MusicResponse> musicResponseList = new ArrayList<>();
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.valueOf("text/plain;charset=utf-8"));
+		headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" +
+			" AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+		ResponseEntity<String> result = restTemplate.exchange(URI, HttpMethod.GET, new HttpEntity<String>(headers),
+			String.class);
+
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		final InputStream stream = new ByteArrayInputStream(result.getBody().getBytes(StandardCharsets.UTF_8));
+		Document doc = documentBuilder.parse(stream);
+		doc.getDocumentElement().normalize();
+		NodeList itemList = doc.getElementsByTagName("item");
+		for (int i = 0; i < itemList.getLength(); i++) {
+			Node node = itemList.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element element = (Element)node;
+				musicResponseList.add(MusicResponse.of(getTagValue("title", element), getTagValue("image", element),
+					getTagValue("name", element)));
+			}
+		}
+
+		return musicResponseList;
+	}
+
+	private String getTagValue(String tag, Element element) {
+		NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+		Node node = (Node)nodeList.item(0);
+		if (node == null)
+			return null;
+		return node.getNodeValue();
+	}
 }
