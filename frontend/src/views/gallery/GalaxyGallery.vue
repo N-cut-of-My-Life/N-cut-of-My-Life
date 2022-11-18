9c67ea04db09ab390812ebb1b1c0f76b13ad1a67
choijoohee213 <choijoohee@naver.com>
2022-11-18 17:59:09 +0900

    feat<FE>: 이미지 자세히보기 모달 추가(css 보완 필요)

--- frontend/src/views/gallery/GalaxyGallery.vue
+++ frontend/src/views/gallery/GalaxyGallery.vue
@@ -1,84 +1,103 @@
 <template>
-  <video muted autoplay loop playbackRate="0.9" style="width: 100%">
-    <source src="@/assets/galaxy.mp4" type="video/mp4" />
-  </video>
-  <div class="other">
-    <b-button
-      @click="router.push({ name: 'introfirstpage' })"
-      class="button_prev"
-      size="sm"
-    >
-      <strong>&lt;</strong>&nbsp;&nbsp;홈페이지
-    </b-button>
-  </div>
+  <div style="display: grid">
+    <video muted autoplay loop playbackRate="0.9" style="width: 100%">
+      <source src="@/assets/galaxy.mp4" type="video/mp4" />
+    </video>
+    <div class="other">
+      <b-button
+        @click="router.push({ name: 'introfirstpage' })"
+        class="button_prev"
+        size="sm"
+      >
+        <strong>&lt;</strong>&nbsp;&nbsp;홈페이지
+      </b-button>
+    </div>
 
-  <!-- <div class="jumbotron"> -->
-  <div class="title">은 하 갤 러 리</div>
-  <audio loop autoplay volume="0.3">
-    <source src="@/assets/audio/mix_gallery.mp3" type="audio/mp3" />
-  </audio>
-  <img class="reloadBtn" @click="reload()" src="@/assets/refresh.png" />
-  <!-- masonry 영역 ver2 -->
-  <MasonryWall
-    :items="[...galleryStore.galleryList].reverse()"
-    :ssr-columns="1"
-    :column-width="200"
-    :gap="16"
-  >
-    <template #default="{ item }">
-      <div class="common" :style="randomBackgroundColor()">
-        <!-- api호출이 아닌 상태이기 떄문에 추후 변경:require 이후 코드 -->
-        <img v-if="item.imgUrl" :src="item.imgUrl" alt="" class="item-image" />
-        <span class="content">{{ item.contents }}</span>
-        <b-row>
-          <b-col cols="8"></b-col>
-          <b-col cols="4">
-            <div class="like" @click="manageLike(item.answerId, item.isMine)">
-              <svg
-                v-if="item.isMine === 'FALSE'"
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-heart"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
-                />
-              </svg>
-              <svg
-                v-else
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-heart-fill"
-                viewBox="0 0 16 16"
-                style="color: red"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-                />
-              </svg>
-              {{ item.like }}
-            </div></b-col
-          >
-        </b-row>
-        <!-- 댓글 공간 -->
-      </div>
-    </template>
-  </MasonryWall>
-  <!-- </div> -->
+    <!-- <div class="jumbotron"> -->
+    <div class="title">은 하 갤 러 리</div>
+    <audio loop autoplay volume="0.3">
+      <source src="@/assets/audio/mix_gallery.mp3" type="audio/mp3" />
+    </audio>
+    <img class="reloadBtn" @click="reload()" src="@/assets/refresh.png" />
+    <div v-if="modalShow == true" class="img-modal">
+      <img class="selected-img" :src="selectedImage" />
+    </div>
+    <img
+      v-if="modalShow == true"
+      class="modal-x-btn"
+      src="@/assets/xButton/x_genie.svg"
+      alt=""
+      @click="toggleModal()"
+    />
+    <!-- masonry 영역 ver2 -->
+    <MasonryWall
+      :items="[...galleryStore.galleryList].reverse()"
+      :ssr-columns="1"
+      :column-width="200"
+      :gap="16"
+    >
+      <template #default="{ item }">
+        <div class="common" :style="randomBackgroundColor()">
+          <!-- api호출이 아닌 상태이기 떄문에 추후 변경:require 이후 코드 -->
+          <img
+            v-if="item.imgUrl"
+            :src="item.imgUrl"
+            alt=""
+            class="item-image"
+            @click="showImgModal(item.imgUrl)"
+          />
+          <span class="content">{{ item.contents }}</span>
+          <b-row>
+            <b-col cols="8"></b-col>
+            <b-col cols="4">
+              <div class="like" @click="manageLike(item.answerId, item.isMine)">
+                <svg
+                  v-if="item.isMine === 'FALSE'"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-heart"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
+                  />
+                </svg>
+                <svg
+                  v-else
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-heart-fill"
+                  viewBox="0 0 16 16"
+                  style="color: red"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                  />
+                </svg>
+                {{ item.like }}
+              </div></b-col
+            >
+          </b-row>
+          <!-- 댓글 공간 -->
+        </div>
+      </template>
+    </MasonryWall>
+  </div>
+  <!-- <div v-if="modalShow == true">
+    안녕하세요!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  </div> -->
 </template>
 
 <script setup>
-import MasonryWall from "@yeger/vue-masonry-wall";
 import { useRouter } from "vue-router";
 import { useGalleryStore } from "@/store/gallery";
 import { useMusicStore } from "@/store/music";
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import { useAccountStore } from "@/store/account";
 
 const userId = useAccountStore().userInfo.userId;
@@ -87,6 +106,9 @@ const router = useRouter();
 const galleryStore = useGalleryStore();
 const GalleryArticles = galleryStore.getGalleryList(userId);
 GalleryArticles;
+
+let modalShow = ref(false);
+let selectedImage = ref("");
 
 // let items = galleryStore.galleryList;
 
@@ -120,9 +142,24 @@ const randomBackgroundColor = () => {
   const idx = Math.floor(Math.random() * colors.length);
   return "background-color: " + colors[idx];
 };
+
+const showImgModal = (imgUrl) => {
+  console.log("!!!hello");
+  toggleModal();
+  selectedImage.value = imgUrl;
+  console.log(modalShow.value);
+  console.log(selectedImage.value);
+};
+
+const toggleModal = () => {
+  modalShow.value = !modalShow.value;
+};
 </script>
 
 <style scoped>
+.body {
+  height: 100vh;
+}
 @font-face {
   font-family: "KyoboHand";
   src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@1.0/KyoboHand.woff")
@@ -206,6 +243,7 @@ video {
 .item-image {
   max-width: 100%;
   border-radius: 0.375rem;
+  cursor: pointer;
 }
 
 .content {
@@ -224,6 +262,85 @@ video {
   cursor: pointer;
   font-family: KyoboHand;
   display: block;
+}
+
+.img-modal {
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  justify-content: center;
+  text-align: center;
+  background-color: #14141495;
+  display: flex;
+  animation: fadein 0.5s;
+  -moz-animation: fadein 0.5s; /* Firefox */
+  -webkit-animation: fadein 0.5s; /* Safari and Chrome */
+  -o-animation: fadein 0.5s;
+}
+
+.selected-img {
+  width: 30%;
+  margin: auto;
+}
+
+.modal-x-btn {
+  cursor: pointer;
+  float: right;
+  position: fixed;
+}
+
+@keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@-moz-keyframes fadein {
+  /* Firefox */
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@-webkit-keyframes fadein {
+  /* Safari and Chrome */
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@-o-keyframes fadein {
+  /* Opera */
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@-moz-keyframes fadein {
+  /* Firefox */
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 </style>
 
